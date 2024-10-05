367171c990d0d138e07bd57e4cba4411a565e5f0
Логинов Владимир <loginov.volodia@gmail.com>
2024-10-05 14:55:58 +0300

    Add project files.

---
+++ FiguresLibrary/FiguresLibrary.cs
@@ -0,0 +1,59 @@
+﻿namespace FiguresLibrary
+{
+    public interface IShape
+    {
+        double CalculateArea();
+    }
+
+    public class Circle : IShape
+    {
+        public double Radius { get; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double CalculateArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+    }
+
+    public class Triangle : IShape
+    {
+        public double SideA { get; }
+        public double SideB { get; }
+        public double SideC { get; }
+
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            SideA = sideA;
+            SideB = sideB;
+            SideC = sideC;
+        }
+
+        public double CalculateArea()
+        {
+            double semiPerimeter = (SideA + SideB + SideC) / 2;
+            return Math.Sqrt(semiPerimeter * (semiPerimeter - SideA) * (semiPerimeter - SideB) * (semiPerimeter - SideC));
+        }
+
+        public bool IsRightAngled()
+        {
+            double[] sides = { SideA, SideB, SideC };
+            Array.Sort(sides);
+            return Math.Abs(Math.Pow(sides[2], 2) - (Math.Pow(sides[0], 2) + Math.Pow(sides[1], 2))) < 0.0001;
+        }
+    }
+
+    public class ShapeCalculator
+    {
+        public double CalculateShapeArea(IShape shape)
+        {
+            return shape.CalculateArea();
+        }
+    }
+
+
+}
